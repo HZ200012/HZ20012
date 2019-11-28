109412998138f1367912f4d67740312d0cac89a6
HZ200012 <57059449+HZ200012@users.noreply.github.com>
2019-11-28 18:40:54 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace WindowsFormsApp2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.游戏选项toolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.开始ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.暂停toolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.重新开始toolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.游戏模式toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.普通模式toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.困难模式toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.炼狱模式toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("仿宋_GB2312", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.DeepPink;
+            this.label1.Location = new System.Drawing.Point(834, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 470);
+            this.label1.TabIndex = 2;
+            this.label1.Tag = "information";
+            this.label1.Text = "游戏说明：\r\nW向上\r\nS向下\r\nA向左\r\nD向右\r\n空格键加速\r\n普通模式：可穿墙，得分100获胜\r\n\r\n困难模式：不可穿墙，得分400获胜\r\n\r\n炼狱模式：不" +
+    "可穿墙，得分700获胜\r\n\r\n\r\n速度等级：极慢，慢，中，快，极快\r\n\r\n默认普通模式";
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelScore.Location = new System.Drawing.Point(833, 60);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(87, 25);
+            this.labelScore.TabIndex = 3;
+            this.labelScore.Tag = "score";
+            this.labelScore.Text = "分数：";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.游戏选项toolStripMenuItem,
+            this.游戏模式toolStripMenuItem1,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1028, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 游戏选项toolStripMenuItem
+            // 
+            this.游戏选项toolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.开始ToolStripMenuItem,
+            this.暂停toolStripMenuItem,
+            this.重新开始toolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.游戏选项toolStripMenuItem.Name = "游戏选项toolStripMenuItem";
+            this.游戏选项toolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.游戏选项toolStripMenuItem.Text = "游戏选项";
+            // 
+            // 开始ToolStripMenuItem
+            // 
+            this.开始ToolStripMenuItem.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.开始ToolStripMenuItem.Name = "开始ToolStripMenuItem";
+            this.开始ToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.开始ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.开始ToolStripMenuItem.Text = "开始";
+            this.开始ToolStripMenuItem.Click += new System.EventHandler(this.开始ToolStripMenuItem_Click);
+            // 
+            // 暂停toolStripMenuItem
+            // 
+            this.暂停toolStripMenuItem.Name = "暂停toolStripMenuItem";
+            this.暂停toolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F2;
+            this.暂停toolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.暂停toolStripMenuItem.Text = "暂停";
+            this.暂停toolStripMenuItem.Click += new System.EventHandler(this.暂停toolStripMenuItem_Click);
+            // 
+            // 重新开始toolStripMenuItem
+            // 
+            this.重新开始toolStripMenuItem.Name = "重新开始toolStripMenuItem";
+            this.重新开始toolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F3;
+            this.重新开始toolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.重新开始toolStripMenuItem.Text = "重新开始";
+            this.重新开始toolStripMenuItem.Click += new System.EventHandler(this.重新开始toolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F4;
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // 游戏模式toolStripMenuItem1
+            // 
+            this.游戏模式toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.普通模式toolStripMenuItem2,
+            this.困难模式toolStripMenuItem3,
+            this.炼狱模式toolStripMenuItem4});
+            this.游戏模式toolStripMenuItem1.Name = "游戏模式toolStripMenuItem1";
+            this.游戏模式toolStripMenuItem1.Size = new System.Drawing.Size(81, 24);
+            this.游戏模式toolStripMenuItem1.Text = "游戏模式";
+            // 
+            // 普通模式toolStripMenuItem2
+            // 
+            this.普通模式toolStripMenuItem2.Name = "普通模式toolStripMenuItem2";
+            this.普通模式toolStripMenuItem2.Size = new System.Drawing.Size(144, 26);
+            this.普通模式toolStripMenuItem2.Text = "普通模式";
+            this.普通模式toolStripMenuItem2.Click += new System.EventHandler(this.普通模式toolStripMenuItem2_Click);
+            // 
+            // 困难模式toolStripMenuItem3
+            // 
+            this.困难模式toolStripMenuItem3.Name = "困难模式toolStripMenuItem3";
+            this.困难模式toolStripMenuItem3.Size = new System.Drawing.Size(144, 26);
+            this.困难模式toolStripMenuItem3.Text = "困难模式";
+            this.困难模式toolStripMenuItem3.Click += new System.EventHandler(this.困难模式toolStripMenuItem3_Click);
+            // 
+            // 炼狱模式toolStripMenuItem4
+            // 
+            this.炼狱模式toolStripMenuItem4.Name = "炼狱模式toolStripMenuItem4";
+            this.炼狱模式toolStripMenuItem4.Size = new System.Drawing.Size(144, 26);
+            this.炼狱模式toolStripMenuItem4.Text = "炼狱模式";
+            this.炼狱模式toolStripMenuItem4.Click += new System.EventHandler(this.炼狱模式toolStripMenuItem4_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(81, 24);
+            this.toolStripMenuItem1.Text = "关于作者";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(213, 6);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(216, 26);
+            this.toolStripMenuItem2.Text = "作者说明";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::贪吃蛇.Properties.Resources.snake;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1028, 709);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "贪吃蛇";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 游戏选项toolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 开始ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 暂停toolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 重新开始toolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 游戏模式toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 普通模式toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem 困难模式toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem 炼狱模式toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//窗口大小1050,650   实际大小除于1.25  791 530
+//游戏边界 left：600，top：480
+//蛇和食物为正方形size20，颜色为红色,食物颜色为黑色
+//top为560和40是碰上下边的条件，left为580和0是碰左右边的条件，location同理
+//移动距离为20
+//Right向右    Up向上      Down向下    Left向右
+namespace WindowsFormsApp2
+{
+	public partial class Form1 : Form
+	{
+        int sum = 0;//第几个蛇块
+		const int GameHeight = 560;//游戏高度
+		const int GameWidth = 600;//游戏宽度
+		const int SIZE = 20;//蛇的宽和高
+        const string W = "Up";
+        const string A = "Left";
+        const string S = "Down";
+        const string D = "Right";
+        static Label[] LabelSum;//蛇块
+        string SpeedLevel;//速度等级
+        Label SnakeHead = new Label();//蛇头初始化
+        Label labelfood;//食物
+         bool Border;//true可以越界，false不可以越界
+        int score;
+        string MoveDirection = D;//初始化移动方向
+        void Restart()//游戏重新开始
+        {
+            if (sum > 0)
+                for (int i = 1; i <= sum; i++)
+                    this.Controls.Remove(LabelSum[i]);
+            SnakeHead.Location = new Point(GameWidth / 2, GameHeight / 2);
+            sum = 0;
+            timer1.Enabled = false;
+        }
+
+        public Form1()
+		{
+			InitializeComponent();
+           LabelSum = new Label[700];//蛇长     （可改变）       
+		}
+        void SnakeMove()//蛇移动
+        {
+            for (int i = sum; i >= 1; i--)
+                LabelSum[i].Location = LabelSum[i - 1].Location;//每一个蛇块的下一个移动位置都是前一个蛇块的之前的位置
+                if (MoveDirection == W)
+                 LabelSum[0].Top -= SIZE;
+            else if (MoveDirection == A)
+                 LabelSum[0].Left -= SIZE;
+            else if (MoveDirection == S)
+                 LabelSum[0].Top += SIZE;
+            else if (MoveDirection == D)
+                 LabelSum[0].Left += SIZE;
+
+        }
+        void EatFood()//蛇头吃食物
+        {
+            if (LabelSum[0].Location == labelfood.Location)
+            {
+                labelfood.Tag = "snake";
+                labelfood.BackColor = Color.Red;
+                LabelSum[++sum] = labelfood;
+                LabelSum[sum].Location = LabelSum[sum - 1].Location;
+                if (MoveDirection == W) LabelSum[sum].Top += SIZE;
+                if (MoveDirection == A) LabelSum[sum].Left += SIZE;
+                if (MoveDirection == S) LabelSum[sum].Top -= SIZE;
+                if (MoveDirection == D) LabelSum[sum].Left -= SIZE;
+                this.Controls.Add(LabelSum[sum]);
+                MakeFood();//造新的食物
+            }
+        }
+        void MakeFood()//生成食物
+        {
+            Random random = new Random();
+            int x, y;//食物出现的位置
+            x = random.Next(GameWidth/SIZE);
+            y = random.Next(2,GameHeight/SIZE);
+            labelfood = new Label();
+            labelfood.Width = labelfood.Height = SIZE;
+            labelfood.Tag = "food";
+            labelfood.BackColor = Color.Black;
+            labelfood.Location = new Point(x * 20, y * 20);
+            this.Controls.Add(labelfood);
+        }
+        void OverBorder(bool judge)//越界判断    ture可以越界,false不可以越界
+        {
+            if (judge){
+                if (LabelSum[0].Top == 20 && MoveDirection == W) LabelSum[0].Top = GameHeight - 20;
+                if (LabelSum[0].Top == GameHeight && MoveDirection == S) LabelSum[0].Top = 40;
+                if (LabelSum[0].Left == 0 && MoveDirection == A) LabelSum[0].Left = GameWidth - SIZE;
+                if (LabelSum[0].Left == GameWidth && MoveDirection == D) LabelSum[0].Left = 0;
+            }
+            else {
+                if ((LabelSum[0].Top == 20 && MoveDirection == W)
+                    || (LabelSum[0].Top == GameHeight && MoveDirection == S)|| (LabelSum[0].Left == GameWidth && MoveDirection == D)||LabelSum[0].Left==-20)
+                    {
+                    timer1.Enabled = false;
+                    if (MessageBox.Show("你输了，" + "你越界了,是否重来", "提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+                        Restart();
+                    else this.Close();
+                    }
+            }
+
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)//改变方向
+        {          
+            if (e.KeyCode == Keys.W) MoveDirection = W;
+            if (e.KeyCode == Keys.A) MoveDirection = A;
+            if (e.KeyCode == Keys.S) MoveDirection = S;
+            if (e.KeyCode == Keys.D) MoveDirection = D;
+            if (e.KeyCode == Keys.Space)
+            {//蛇加速
+                if ((timer1.Interval -100)== 0) timer1.Interval = 500;
+                else timer1.Interval -= 100;
+            }
+        }
+        bool SnakeTouch()//碰到自己
+        {
+            for (int i = 1; i <= sum; i++)
+                if (LabelSum[0].Location == LabelSum[i].Location) return true;//代表碰到自己
+            return false;//没有碰到自己
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Border = true;
+            score = 100;//胜利得分
+            Label LabelRightLine = new Label();
+            Label labelDownLine = new Label();
+            Label LabelTopLine = new Label();
+            LabelRightLine.Width = 1;
+            LabelRightLine.Height = 521;
+            labelDownLine.Height = 1;
+            labelDownLine.Width = 600;
+            LabelTopLine.Height = 1;
+            LabelTopLine.Width = 600;
+            LabelRightLine.BackColor = Color.Black;
+            labelDownLine.BackColor = Color.Black;
+            LabelTopLine.BackColor = Color.Black;
+            LabelTopLine.Top = 40;
+            LabelRightLine.Left = GameWidth;
+            LabelRightLine.Top = 40;
+            labelDownLine.Top = GameHeight;
+            this.Controls.Add(labelDownLine);
+            this.Controls.Add(LabelRightLine);
+            this.Controls.Add(LabelTopLine);//右分界线和下分界线的设置
+            
+        
+            SnakeHead.Tag = "snakehead";
+            SnakeHead.BackColor = Color.Red;
+            SnakeHead.Location = new Point(GameWidth / 2, GameHeight / 2);
+            SnakeHead.Width = SnakeHead.Height = SIZE;
+            SnakeHead.Font = new System.Drawing.Font("宋体", 18F);
+            SnakeHead.Text = "→";
+            SnakeHead.TextAlign=ContentAlignment.MiddleCenter;
+
+            this.Controls.Add(SnakeHead);
+                                            /*Label SnakeHead = new Label(){
+                                                 BackColor=Color.Red;  
+                                                      };         这种初始化方式会出现未知的错误*/
+            LabelSum[sum] = SnakeHead;//初始化蛇头
+            MakeFood();//初始化第一个食物
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+                switch (timer1.Interval)
+                {
+                    case 500: SpeedLevel = "极慢"; break;
+                    case 400: SpeedLevel = "慢"; break;
+                    case 300: SpeedLevel = "中"; break;
+                    case 200: SpeedLevel = "快"; break;
+                    case 100: SpeedLevel = "极快"; break;
+                }
+            if (MoveDirection == W) SnakeHead.Text = "↑";
+            if (MoveDirection == A) SnakeHead.Text = "←";
+            if (MoveDirection == S) SnakeHead.Text = "↓";
+            if (MoveDirection == D) SnakeHead.Text = "→";
+            SnakeMove();
+            if (SnakeTouch())
+            {
+                timer1.Enabled = false;
+                if (MessageBox.Show("你输了，你碰到了蛇身,是否重来", "提示",MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+                    Restart();
+                else this.Close();
+            }
+            EatFood();
+            OverBorder(Border);
+            labelScore.Text = "分数：" + sum + "\n当前速度:" + SpeedLevel;
+            if (sum == score) {
+                timer1.Enabled = false;
+                MessageBox.Show("恭喜你，你赢了" , "提示");                
+                Close();
+               }
+        }
+        private void button3_Click(object sender, EventArgs e)//刷新窗体
+        {
+            Restart();
+        }
+
+        private void 开始ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void 暂停toolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void 重新开始toolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Restart();
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void 普通模式toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+            score = 100;
+            Border = true;
+        }
+
+        private void 困难模式toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            score = 400;
+            Border = false;
+        }
+
+        private void 炼狱模式toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            score = 700;
+            Border = false;
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("作者是一位英气逼人的帅哥，自己呕心沥血做出了这个小游戏，" +
+                "决不允许其他人利用这个小游戏，" +
+                "如果有人拿这个东西去为自己谋取利益让作者发现了，" +
+                "作者一定会不远万里取你狗命！！！", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
